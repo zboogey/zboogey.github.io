88ff3ed8f485c202cb9f38343e470e807472fc63
Almat <ybraio8@gmail.com>
2026-09-24 12:16:57 +0500

    fix: update base path to root

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
+    react({
+      babel: { presets: [reactCompilerPreset()] }
+    })
   ],
-  base: '/https://github.com/zboogey/zboogey.github.io//',
+  base: '/',
 })
